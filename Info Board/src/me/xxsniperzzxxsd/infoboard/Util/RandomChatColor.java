6bf76a587c4983c7aae13c5cf19cb6cee4986265
SNiPeRzCiinema <bimmr6696@gmail.com>
2013-12-02 13:13:55 -0500

    - Fixed Infected Support - Updated Random Chat Color class

--- Info Board/src/me/xxsniperzzxxsd/infoboard/Util/RandomChatColor.java
+++ Info Board/src/me/xxsniperzzxxsd/infoboard/Util/RandomChatColor.java
@@ -4,24 +4,40 @@ package me.xxsniperzzxxsd.infoboard.Util;
 import java.util.Random;
 
 import org.bukkit.ChatColor;
-
+/**
+ * If anyone wants to use this class for their own plugin go ahead, this is a class you can also find in my gists(On Github).
+ * If you have any suggestions/additions that you think would help Infected feel free to message me or fork the gist
+ * 
+ * @author Sniperz
+ *
+ */
 
 public class RandomChatColor {
 
-	public static ChatColor getColor() {
+	public static ChatColor getColor(ChatColor...chatColors ) {
 		Random r = new Random();
-		int i = r.nextInt(ChatColor.values().length);
-		while (!ChatColor.values()[i].isColor())
-			i = r.nextInt(ChatColor.values().length);
-		ChatColor rc = ChatColor.values()[i];
+		ChatColor[] colors;
+		if(chatColors.length == 0)
+			colors = ChatColor.values();
+		else
+			colors = chatColors;
+		int i = r.nextInt(colors.length);
+		while (!colors[i].isColor())
+			i = r.nextInt(colors.length);
+		ChatColor rc = colors[i];
 		return rc;
 	}
-	public static ChatColor getFormat() {
+	public static ChatColor getFormat(ChatColor...chatColors ) {
 		Random r = new Random();
-		int i = r.nextInt(ChatColor.values().length);
-		while (!ChatColor.values()[i].isFormat())
-			i = r.nextInt(ChatColor.values().length);
-		ChatColor rc = ChatColor.values()[i];
+		ChatColor[] colors;
+		if(chatColors.length == 0)
+			colors = ChatColor.values();
+		else
+			colors = chatColors;
+		int i = r.nextInt(colors.length);
+		while (!colors[i].isFormat())
+			i = r.nextInt(colors.length);
+		ChatColor rc = colors[i];
 		return rc;
 	}
 }
--- Info Board/src/me/xxsniperzzxxsd/infoboard/Variables/InfectedVariables.java
+++ Info Board/src/me/xxsniperzzxxsd/infoboard/Variables/InfectedVariables.java
@@ -1,7 +1,10 @@
 
 package me.xxsniperzzxxsd.infoboard.Variables;
 
-import me.xxsniperzzxx_sd.infected.Infected;
+import me.sniperzciinema.infected.Handlers.Lobby;
+import me.sniperzciinema.infected.Handlers.Player.InfPlayer;
+import me.sniperzciinema.infected.Handlers.Player.InfPlayerManager;
+import me.sniperzciinema.infected.Tools.Settings;
 
 import org.bukkit.entity.Player;
 
@@ -10,17 +13,24 @@ public class InfectedVariables {
 
 	public static String replaceVariables(String string, Player player) {
 		String newString = string;
+		InfPlayer ip = InfPlayerManager.getInfPlayer(player);
 
 		if (newString.contains("<infectedpoints>"))
-			newString = newString.replaceAll("<infectedpoints>", String.valueOf(Infected.playerGetPoints(player.getName())));
+			newString = newString.replaceAll("<infectedpoints>", String.valueOf(ip.getPoints(Settings.VaultEnabled())));
 		if (newString.contains("<infectedscore>"))
-			newString = newString.replaceAll("<infectedscore>", String.valueOf(Infected.playerGetScore(player.getName())));
+			newString = newString.replaceAll("<infectedscore>", String.valueOf(ip.getScore()));
 		if (newString.contains("<infecteddeaths>"))
-			newString = newString.replaceAll("<infecteddeaths>", String.valueOf(Infected.playerGetDeaths(player.getName())));
+			newString = newString.replaceAll("<infecteddeaths>", String.valueOf(ip.getDeaths()));
 		if (newString.contains("<infectedkills>"))
-			newString = newString.replaceAll("<infectedkills>", String.valueOf(Infected.playerGetKills(player.getName())));
+			newString = newString.replaceAll("<infectedkills>", String.valueOf(ip.getKills()));
+		if (newString.contains("<infectedhighestkills>"))
+			newString = newString.replaceAll("<infectedhighestkills>", String.valueOf(ip.getHighestKillStreak()));
 		if (newString.contains("<infectedgamestate>"))
-			newString = newString.replaceAll("<infectedgamestate>", String.valueOf(Infected.getGameState().getStatus()));
+			newString = newString.replaceAll("<infectedgamestate>", String.valueOf(Lobby.getGameState().toString()));
+		if (newString.contains("<infectedactivearena>"))
+			newString = newString.replaceAll("<infectedactivearena>", String.valueOf(Lobby.getActiveArena().getName()));
+		if (newString.contains("<infectedplayers>"))
+			newString = newString.replaceAll("<infectedplayers>", String.valueOf(Lobby.getInGame().size()));
 
 		return newString;
 	}
